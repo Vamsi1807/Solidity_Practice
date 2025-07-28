fd9a6c21c3a082403de34aced25b42ef055479b7
Vamsi1807 <22bq1a1249@vvit.net>
2025-07-28 09:19:20 +0530

    name changed

--- contracts/MethodOverloading.sol
+++ contracts/MethodOverloading.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier:GPL-3.0
 pragma solidity ^0.8.14;
 
-contract FunctionDemo{
+contract MethodOverloading{
     int a=10;
     function getSum(int b,int c)public view returns(int){
         int sum = a+b+c;
