a134522874e7f3096d4be3b6020cee771afc2a1d
Vamsi1807 <22bq1a1249@vvit.net>
2025-08-11 09:07:32 +0530

    String programme added

---
+++ contracts/LabCycle/StringExample.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier:GPL-3.0
+pragma solidity ^0.8.14;
+
+contract StringExample{
+    string public s1;
+    string public s2;
+
+    function setValues(string memory _s1, string memory _s2) public {
+        s1 = _s1;
+        s2 = _s2;
+    }
+
+    function retrieveValues()public view returns(string memory, string memory){
+        return (s1,s2);
+    }
+
+    function concatenate()public view returns(string memory){
+        return string.concat(s1," and ",s2);
+    }
+
+    function concatenateString()public view returns(string memory){
+        return (string(abi.encodePacked(s1," and ",s2)));
+    }
+
+    function compare() public view returns(bool){
+        return(keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2)));
+    }
+}
